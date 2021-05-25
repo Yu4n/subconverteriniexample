d41b660193601b238ca0179cb0778f351b11537f
Yu4n <28648731+yu4n@users.noreply.github.com>
2021-05-25 19:21:36 +0800

    rename.py

---
+++ scripts/rename.py
@@ -0,0 +1,13 @@
+import base64
+import json
+
+with open('VmsLines.txt', 'r') as f:
+    urls = [base64.b64decode(line.strip()[8:]).decode("utf-8") for line in f]
+i = 1
+for url in urls:
+    url = json.loads(url)
+    url["ps"] = url["ps"][:3] + str(i)
+    url = str(url)
+    i += 1
+    url = base64.b64encode(bytes(url, "utf-8"))
+    print("vmess://" + url.decode())
