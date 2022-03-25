aa6646fbbec557720461111161e894cd682214ac
Yu4n <28648731+Yu4n@users.noreply.github.com>
2022-03-25 21:04:13 +0800

    Delete getContents.py

--- scripts/getContents.py
+++
@@ -1,9 +0,0 @@
-import requests
-with open('clash.txt', 'r') as f:
-    urls = [line.strip() for line in f]
-open("LinesToGet.txt", 'w').close()
-wt = open("LinesToGet.txt", 'a', encoding='utf-8')
-for x in urls:
-    c = requests.get(x)
-    wt.write(c.text)
-wt.close()
